9f26afd91914ffcbcad0e6710a76bdf40ba918f3
Rene Ichin <reneichin@iMac-de-Rene.local>
2024-05-30 13:25:24 -0600

    cambios en base se quito la url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
     site: 'https://RemyIchin.github.io',
-    base:'https://reneichin.github.io/RemyIchin.github.io/',
+    // base:'https://reneichin.github.io/RemyIchin.github.io/',
   });
