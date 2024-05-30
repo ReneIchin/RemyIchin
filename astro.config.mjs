06b0f2ec0244f9aa3efd0c1cafdb7a1cefffb8f2
Rene Ichin <reneichin@iMac-de-Rene.local>
2024-05-29 18:05:46 -0600

    change de base 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
     site: 'https://RemyIchin.github.io',
-    base:'https://RemyIchin.github.io',
+    base:'/https://RemyIchin.github.io/',
   });
