b9e92570f8485fcc1727e9da3df91defd6fced65
Rene Ichin <reneichin@iMac-de-Rene.local>
2024-05-30 16:45:32 -0600

    cambios en rutas de los componentes y de mas

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-    site: 'https://reneichin.github.io/RemyIchin.github.io',
-    base:'https://reneichin.github.io/RemyIchin.github.io',
-  });
-
 // import { defineConfig } from 'astro/config';
 
-// const isProduction = process.env.NODE_ENV === 'production';
-
 // export default defineConfig({
-//   site: isProduction ? 'https://reneichin.github.io/RemyIchin.github.io' : 'http://localhost:3000',
-//   base: isProduction ? '/RemyIchin.github.io/' : '/',
-// });
+//     site: 'https://reneichin.github.io/RemyIchin.github.io',
+//     base:'https://reneichin.github.io/RemyIchin.github.io',
+//   });
+
+import { defineConfig } from 'astro/config';
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+export default defineConfig({
+  site: isProduction ? 'https://reneichin.github.io/RemyIchin.github.io' : 'http://localhost:3000',
+  base: isProduction ? '/RemyIchin.github.io/' : '/',
+});
