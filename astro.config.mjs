a010d1c34de8758d5282493ec4bbf0ad60e3cce2
Rene Ichin <reneichin@iMac-de-Rene.local>
2024-05-30 16:33:14 -0600

    cambios nuevamente de las rutas

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-    site: 'https://RemyIchin.github.io',
+    site: 'https://reneichin.github.io/RemyIchin.github.io',
     base:'https://reneichin.github.io/RemyIchin.github.io',
   });
 
